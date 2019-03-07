9d9d2a2c8c6477c8bc6647ffbeef0fa600f0005f
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-03-07 12:03:47 +0300

    Add project files.

---
+++ netcore3_dotnetbuildhangs/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary2 {
+    public class Class1 {
+        class ListItem {
+            public ListItem Prev { get; set; }
+            public ListItem Next { get; set; }
+        }
+        void ArrangeNonOverlapping(IList<ListItem> labels) {
+            List<ListItem> actualLabels = new List<ListItem>(labels);
+            ListItem prev = null;
+            ListItem next = null;
+            for(int i = 0; i < labels.Count; i++) {
+                ListItem label = actualLabels[i];
+                label.Prev = prev;
+                label.Next = next;
+                if(prev != null)
+                    prev.Next = label;
+                if(next != null)
+                    next.Prev = label;
+            }
+        }
+    }
+}
